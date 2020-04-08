fbd07dc02e3df2eae2511d5002ef2985ab44522f
liam <liam.gerstein@gmail.com>
2020-04-08 11:17:31 +0200

    Check LiveServer And Wamp

--- SITES/UFO1_PHP/index.php
+++ SITES/UFO1_PHP/index.php
@@ -1,6 +1,6 @@
 <!--                             
    UFO DATABASE   ufodb_banner.png
-   
+   http://localhost/webdev/SITES/UFO1_PHP/
    --> 
    <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <HTML>
