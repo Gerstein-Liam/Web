da8c5476f1c3c2a83c74bdff397c31283c8c6cfe
liam <liam.gerstein@gmail.com>
2020-04-12 22:24:13 +0200

    modal text

--- SITES/UFOV2/FRONTEND/js/startup.js
+++ SITES/UFOV2/FRONTEND/js/startup.js
@@ -79,7 +79,43 @@ function  includeHTML()  {
 
 
 
+var modal;
+var btn;
+var span;
+
+
+
+
+
  function Startup() {
+   modal = document.getElementById("myModal");
+
+   // Get the button that opens the modal
+  btn = document.getElementById("PersonFilter");
+   
+   // Get the <span> element that closes the modal
+  span = document.getElementsByClassName("close")[0];
+   
+
+   // When the user clicks the button, open the modal 
+btn.onclick = function() {
+   modal.style.display = "block";
+ }
+ 
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function() {
+   modal.style.display = "none";
+ }
+ 
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ }
+
+
+
    // includeHTML() ;
   //  myMove();
   }
