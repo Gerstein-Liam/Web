f62cc3d97e25d96b827961538fa63bf036b45b32
liam <liam.gerstein@gmail.com>
2020-04-13 18:58:44 +0200

    make fonction to laod file

--- ASPECT/BACKEND/nodejs/readfile/readfile.js
+++ ASPECT/BACKEND/nodejs/readfile/readfile.js
@@ -1,3 +1,4 @@
+/*
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
@@ -6,4 +7,30 @@ http.createServer(function (req, res) {
     res.write(data);
     res.end();
   });
+}).listen(8080);
+*/
+function LoadHTML(req, res, filepath) {
+  fs.readFile(filepath, function (err, data) {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    res.end();
+  });
+}
+
+
+
+var http = require('http');
+var fs = require('fs');
+http.createServer(function (req, res) {
+  if (req.method === "GET") {
+    console.log("GetMethod");
+    if (req.url === ("/")) {
+      LoadHTML(req, res, 'demofile1.html');
+    }
+    else {
+      if (req.url === ("/other")) {
+        LoadHTML(req, res, 'otherfile.html');
+      }
+    }
+  }
 }).listen(8080);
