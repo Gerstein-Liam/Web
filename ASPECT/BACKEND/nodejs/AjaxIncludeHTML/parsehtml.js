661cd865282116de7f005253f11f0652afc3e2d3
liam <liam.gerstein@gmail.com>
2020-04-14 15:04:34 +0200

    Create_new_Index

--- ASPECT/BACKEND/nodejs/AjaxIncludeHTML/parsehtml.js
+++ ASPECT/BACKEND/nodejs/AjaxIncludeHTML/parsehtml.js
@@ -3,49 +3,58 @@ const { Console } = require('console');
 var filepath = "index.html"
 var balise_prefixe = "include-html";
 var balise_entiere;
-
-var finalhtml= CreateHTML("index.html");
-
-var str;
+var myHTML;
+let FinalHTML="";
 
 
-fs.readFile(filepath, function (err, data)
-{
+console.log("-------------START------------------");
 
-    CreateHTML(data.toString());
-    
-});
-   
-
-
-
-
-
-
-function CreateHTML(str){
-        var TagBeginDefine = "<div include-html=\"";
-        var TagEndDefine = "\"></div>";
-        var TagBeginIndex;
-        var TagEndIndex;
-        var FullTag;
-        var FileNameBeginIndex;
-        var FileNameEndIndex;
-        var FileName;
-        var TmpPART;
-        if (str.includes(TagBeginDefine)) {
-            TagBeginIndex = str.search(TagBeginDefine);
-            TmpPART = str.substring(TagBeginIndex, (TagBeginIndex + 100));
-            console.log("Portion="+TmpPART );
-            TagEndIndex = str.search(TagEndDefine) + TagEndDefine.length;
-            FullTag = str.substring(TagBeginIndex, TagEndIndex);
-            console.log("Full Tag=== " + FullTag);
-            FileName = str.substring((str.search(TagBeginDefine) + TagBeginDefine.length), str.search(TagEndDefine));
-            console.log("FileName=== " + FileName.substring);
-            fs.readFile(FileName, function (err, incdata) {
-                var include= incdata.toString();
-                str=str.replace( FullTag, include);
-                console.log(str);
-                CreateHTML(str);
-            });
- }
+function CreateHTML(str) {
+    var TagBeginDefine = "<div include-html=\"";
+    var TagEndDefine = "\"></div>";
+    var TagBeginIndex;
+    var TagEndIndex;
+    var FullTag;
+    var FileNameBeginIndex;
+    var FileNameEndIndex;
+    var FileName;
+    var TmpPART;
+    if (str.includes(TagBeginDefine)) {
+        TagBeginIndex = str.search(TagBeginDefine);
+        TmpPART = str.substring(TagBeginIndex, (TagBeginIndex + 100));
+        //      console.log("Portion="+TmpPART );
+        TagEndIndex = str.search(TagEndDefine) + TagEndDefine.length;
+        FullTag = str.substring(TagBeginIndex, TagEndIndex);
+        //      console.log("Full Tag=== " + FullTag);
+        FileName = str.substring((str.search(TagBeginDefine) + TagBeginDefine.length), str.search(TagEndDefine));
+        //      console.log("FileName=== " + FileName.substring);
+        var include = fs.readFileSync(FileName, "utf-8");
+        str = str.replace(FullTag, include);
+        // console.log(str);
+        return CreateHTML(str);
+    }
+    else { 
+        //console.log(str);
+        //console.log(FinalHTML);
+        return str;
+       // FinalHTML=str;
+         //console.log(FinalHTML);
+     }
 }
+try{
+    myHTML=fs.readFileSync(filepath, "utf-8");
+}
+catch(err){
+    console.log("error");
+}
+finally{
+
+    FinalHTML=CreateHTML(myHTML);
+}
+
+console.log(FinalHTML);
+
+fs.writeFileSync("buildeldhtml.html",FinalHTML);
+
+
+console.log("-------------END------------------");
