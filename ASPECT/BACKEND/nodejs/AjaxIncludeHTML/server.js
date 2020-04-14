8e7f5957af22ec490ef39e01425e17e7d62e78bf
liam <liam.gerstein@gmail.com>
2020-04-14 20:28:27 +0200

    refresh with F5

--- ASPECT/BACKEND/nodejs/AjaxIncludeHTML/server.js
+++ ASPECT/BACKEND/nodejs/AjaxIncludeHTML/server.js
@@ -1,33 +1,14 @@
-
-
 var htmlBuilder= require("./htmlBuilder");
-
 htmlBuilder.HTMLBuild("index_seed.html");
 
-
-
-
-
-
-
-
-function LoadHTML(req, res, filepath) {
+function HTML_Query(req, res, filepath) {
   fs.readFile(filepath, function (err, data) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    str=data.toString();
-    
-
-
-    console.log(str);
-    
-  
-
-    console.log(str.includes("w3-include-html"));
     res.write(data);
     res.end();
   });
 }
-function LoadJS(req, res, filepath) {
+function JSLIB_Query(req, res, filepath) {
  //fs.readFile('client_ajax.js', function (err, data) {
   fs.readFile(filepath, function (err, data) {
    if(err){
@@ -41,63 +22,59 @@ function LoadJS(req, res, filepath) {
     }
   });
 }
+function CSS_Query(req, res, filepath) {
+  //fs.readFile('client_ajax.js', function (err, data) {
+   fs.readFile(filepath, function (err, data) {
+    if(err){
+     console.log("ErrorOccured");
+     }
+     else
+     {
+       res.writeHead(200, { 'Content-Type': 'text/css' });
+       res.write(data);
+       res.end();
+     }
+   });
+ }
 var http = require('http');
 var fs = require('fs');
 http.createServer(function (req, res) {
+
+  //To permit refresh
+  htmlBuilder.HTMLBuild("index_seed.html");
+
   console.log(req.url.substring(1));
   if (req.method === "GET") {
     //console.log("GetMethod");
     if (req.url === ("/")) {
       console.log("LoadIndex");
-      LoadHTML(req, res, 'index.html');
-    }
-    else {
-      if (req.url === ("/i_from_index.html")) {
-        console.log("IncFromIndex");
-        LoadHTML(req, res, 'i_from_index.html');
-      }
-      else
+      HTML_Query(req, res, 'index.html');
+    }else
+    {
+      if (req.url.includes(".html")) {
+        console.log("Client request for HTML document");
+      }else
       {
-        
-        
-        if (req.url === ("/i_from_include.html")) {
-          console.log("IncFromInclude");
-          LoadHTML(req, res, req.url.substring(1));
-        }
-        else
+        if (req.url.includes(".js")) {
+          JSLIB_Query(req, res, req.url.substring(1));
+          console.log("Client request for JAVASCRIPT document");
+        }else
         {
-         // console.log("IncFromInclude");
-         // console.log("Look for ClientJS");
-          LoadJS(req, res, req.url.substring(1));
-        
-        
+          if (req.url.includes(".css")) {
+            CSS_Query(req, res, req.url.substring(1));
+            console.log("Client request for JAVASCRIPT document");
+          }else
+          {
+            console.log("Client request for other (Image,etc)");
+          }
         }
-        
-       
-      
-      
-      
-      
       }
     }
   }
 }).listen(8080);
-
-
-
-
-
-
-
-
 /*
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function (err, data) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    res.end();
-  });
-}).listen(8080);
+if (req.url === ("/")) {
+  console.log("LoadIndex");
+  HTML_Query(req, res, 'index.html');
+}
 */
