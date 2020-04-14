1a0b8bde705ad6ea06fa353e0b77a4a794bff9a3
liam <liam.gerstein@gmail.com>
2020-04-14 15:13:32 +0200

    Created a separate folder for the builder

---
+++ ASPECT/BACKEND/nodejs/HTMLBuilder_V1/parsehtml.js
@@ -0,0 +1,49 @@
+var fs = require('fs');
+const { Console } = require('console');
+var filepath = "index_seed.html"
+var TemplatedHTML;
+var FinalHTML="";
+console.log("-------------START------------------");
+function GenerateHTML_ForClient(str) {
+    var TagBeginDefine = "<div include-html=\"";
+    var TagEndDefine = "\"></div>";
+    var TagBeginIndex;
+    var TagEndIndex;
+    var FullTag;
+    var FileName;
+    var TmpPART;
+    if (str.includes(TagBeginDefine)) {
+        TagBeginIndex = str.search(TagBeginDefine);
+        TmpPART = str.substring(TagBeginIndex, (TagBeginIndex + 100));
+        //      console.log("Portion="+TmpPART );
+        TagEndIndex = str.search(TagEndDefine) + TagEndDefine.length;
+        FullTag = str.substring(TagBeginIndex, TagEndIndex);
+        //      console.log("Full Tag=== " + FullTag);
+        FileName = str.substring((str.search(TagBeginDefine) + TagBeginDefine.length), str.search(TagEndDefine));
+        //      console.log("FileName=== " + FileName.substring);
+        var include = fs.readFileSync(FileName, "utf-8");
+        str = str.replace(FullTag, include);
+        // console.log(str);
+        return GenerateHTML_ForClient(str);
+    }
+    else { 
+        //console.log(str);
+        //console.log(FinalHTML);
+        return str;
+       // FinalHTML=str;
+         //console.log(FinalHTML);
+     }
+}
+try{
+    TemplatedHTML=fs.readFileSync(filepath, "utf-8");
+}
+catch(err){
+    console.log("error");
+}
+finally{
+    FinalHTML=GenerateHTML_ForClient(TemplatedHTML);
+}
+console.log(FinalHTML);
+fs.writeFileSync("index.html",FinalHTML);
+console.log("-------------END OF Creation------------------");
+console.log("-------------Now created server------------------");
