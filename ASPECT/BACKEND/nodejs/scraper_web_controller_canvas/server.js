3097ecf5a2e26343aac80d4376813587556378c2
liam <liam.gerstein@gmail.com>
2020-04-15 12:44:29 +0200

    parse correctly the URL

--- ASPECT/BACKEND/nodejs/scraper_web_controller_canvas/server.js
+++ ASPECT/BACKEND/nodejs/scraper_web_controller_canvas/server.js
@@ -1,11 +1,12 @@
 var http = require('http');
+var url = require('url');
 var htmlBuilder= require("./custom_node_modules/htmlBuilder");
 var hq= require("./custom_node_modules/html_queries");
 var capi= require("./custom_node_modules/api_request");
 htmlBuilder.HTMLBuild("index_seed.html");     //On crée le fichier HTML generé en debut d'execution (bloquand)
 http.createServer(function (req, res) {
   htmlBuilder.HTMLBuild("index_seed.html");   //On refresh tout le contenu HTML, comme ca tu peux developper le HTML,CSS,Javascript coté client,  sans redemarrer le serveur
-  console.log(req.url.substring(1));
+ // console.log(req.url.substring(1));
   if (req.method === "GET") {
     if (req.url.includes("API")) {
       console.log("Client request console api");
@@ -13,27 +14,29 @@ http.createServer(function (req, res) {
     }else
     {
       if (req.url === ("/")){
-        console.log("LoadIndex");
+        console.log("request of index.html :  URL=" +  req.url.substring(1));
         hq.htmlqueries.HTML_Query(req, res, 'index.html');
       }
       else
       {
         if (req.url.includes(".html")) {
-          console.log("Client request for HTML document");
+          console.log("Client request for HTML document,other than index.html :  URL=" +  req.url.substring(1));
         }
         else
         {
           if (req.url.includes(".js")) {
+            console.log("Client request for JAVASCRIPT document : URL="+  req.url.substring(1));
             hq.htmlqueries.JSLIB_Query(req, res, req.url.substring(1));
-            console.log("Client request for JAVASCRIPT document");
+            
           }else
           {
             if (req.url.includes(".css")) {
+              console.log("Client request for CSS document   :  URL=" +  req.url.substring(1));
               hq.htmlqueries.CSS_Query(req, res, req.url.substring(1));
-              console.log("Client request for JAVASCRIPT document");
+             
             }else
             {
-              console.log("Client request for other (Image,etc)");
+              console.log("Client request for other (Image,etc) ,TO DO :  URL=" +  req.url.substring(1));
             }
           }
         }
