63e916500f4fdb3e69ecd526cc8539690cd39282
liam <liam.gerstein@gmail.com>
2020-04-20 23:22:16 +0200

    recuperation des donnes a lancement, encore a mettre en forme

--- SITES/UFOV2/FRONTEND/js/admin.js
+++ SITES/UFOV2/FRONTEND/js/admin.js
@@ -1,9 +1,6 @@
 AddPersonModal= new Modal("AddPersonModal");
-
-
-
+//https://www.w3schools.com/js/js_objects.asp
 function RequestAPI_PostMethod(id, url ,parameters) {
-   
     console.log("requested URL: " + url);
     if (window.XMLHttpRequest) {
         // code for IE7+, Firefox, Chrome, Opera, Safari
@@ -12,13 +9,9 @@ function RequestAPI_PostMethod(id, url ,parameters) {
         // code for IE6, IE5
         xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
     }
-
-   
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
-
-        
             document.getElementById(id).innerHTML = this.responseText;
         }
     };
@@ -26,28 +19,40 @@ function RequestAPI_PostMethod(id, url ,parameters) {
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xmlhttp.send(parameters);
 }
-
-
 function AddPerson(){
-
-
-
-
-
 let lastname = document.getElementById("lastname").value;
 let firstname = document.getElementById("firstname").value;
 let fonction  = document.getElementById("fonction").value;
-
 let domain = document.getElementById("domain").value;
 let country = document.getElementById("country").value;
 let implication = document.getElementById("implication").value;
 let position = document.getElementById("position").value;
-
 var paras = `command=addperson&LASTNAME=${lastname}&FIRSTNAME=${firstname}&FONCTION=${fonction}&DOMAIN=${domain}&COUNTRY=${country}&IMPLICATION=${implication}&POSITION=${position} `;
-
 console.log(`LASTNAME= ${lastname}  FIRSTNAME=${firstname}  FONCTION= ${fonction} `);
 console.log(`DOMAIN= ${domain}  COUNTRY=${country}  IMPLICATION= ${implication}  POSITION ${position} `);
-
 RequestAPI_PostMethod("save_status", "/API" ,paras);
+}
 
+
+
+
+function RequestAPI_PostMethod_LoadPersonList(url ,parameters) {
+    console.log("requested URL: " + url);
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            console.log(obj);
+            //document.getElementById(id).innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send(parameters);
 }
--- SITES/UFOV2/FRONTEND/js/startup.js
+++ SITES/UFOV2/FRONTEND/js/startup.js
@@ -1,6 +1,8 @@
 // Fonctions a exectuer au démaragge (AttachModal quand app sur server)
 function Startup() {
    DisableRightClickMenuContext();
+   var paras = "command=loadpersonlist";
+   RequestAPI_PostMethod_LoadPersonList( "/API" ,paras)
 }
 // Desactive le menu contextuel par défaut du click droit
 function DisableRightClickMenuContext() {
