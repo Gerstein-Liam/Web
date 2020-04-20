f24642e2f169c862a9018c7e9bf639aaf95148fd
liam <liam.gerstein@gmail.com>
2020-04-21 00:32:23 +0200

    continue to fill table

--- SITES/UFOV2/FRONTEND/js/admin.js
+++ SITES/UFOV2/FRONTEND/js/admin.js
@@ -1,5 +1,6 @@
 AddPersonModal= new Modal("AddPersonModal");
 //https://www.w3schools.com/js/js_objects.asp
+//https://www.geeksforgeeks.org/remove-all-the-child-elements-of-a-dom-node-in-javascript/
 function RequestAPI_PostMethod(id, url ,parameters) {
     console.log("requested URL: " + url);
     if (window.XMLHttpRequest) {
@@ -13,6 +14,7 @@ function RequestAPI_PostMethod(id, url ,parameters) {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText);
             document.getElementById(id).innerHTML = this.responseText;
+            //ReloadPersonTable();
         }
     };
     xmlhttp.open("POST", url, true);
@@ -32,10 +34,63 @@ console.log(`LASTNAME= ${lastname}  FIRSTNAME=${firstname}  FONCTION= ${fonction
 console.log(`DOMAIN= ${domain}  COUNTRY=${country}  IMPLICATION= ${implication}  POSITION ${position} `);
 RequestAPI_PostMethod("save_status", "/API" ,paras);
 }
-
-
-
-
+function RequestAPI_PostMethod_LoadPersonList(url ,parameters) {
+    console.log("requested URL: " + url);
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var obj = JSON.parse(this.responseText);
+            console.log(obj);
+            var result = "";
+            var table = document.getElementById("customers");
+           var ligne;
+            for (let i = 0; i <= (obj.length - 1); i++) {
+              var ligne = document.createElement("tr");
+              ligne.setAttribute('onmousedown','OpenPersonRightClickModal(event)')
+              var _firstname = document.createElement("td");
+              _firstname.innerHTML = obj[i].Firstname;
+              var _lastname = document.createElement("td");
+              _lastname.innerHTML = obj[i].Name;
+              var _fonction = document.createElement("td");
+              _fonction.innerHTML = obj[i].Fonction;
+              var _domain = document.createElement("td");
+              _domain.innerHTML = obj[i].Domain;
+              var _country = document.createElement("td");
+              _country.innerHTML = obj[i].Country;
+              var _implication = document.createElement("td");
+              _implication.innerHTML = obj[i].Implication;
+              var _position = document.createElement("td");
+              _position.innerHTML = obj[i].Position;
+              ligne.appendChild(_firstname);
+              ligne.appendChild(_lastname);
+              ligne.appendChild(_fonction);
+              ligne.appendChild(_domain);
+              ligne.appendChild(_country);
+              ligne.appendChild(_implication);
+              ligne.appendChild(_position);
+              console.log(obj[i].Firstname + obj[i].Name);
+              result += obj[i].Firstname + " " + obj[i].Name;
+              table.appendChild(ligne);
+            }
+          //  dom.appendChild(table);
+         //   console.log(result);
+        }
+    };
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send(parameters);
+}
+function ReloadPersonTable(){
+    var paras = "command=loadpersonlist";
+    RequestAPI_PostMethod_LoadPersonList( "/API" ,paras);
+}
+/*
 function RequestAPI_PostMethod_LoadPersonList(url ,parameters) {
     console.log("requested URL: " + url);
     if (window.XMLHttpRequest) {
@@ -55,4 +110,4 @@ function RequestAPI_PostMethod_LoadPersonList(url ,parameters) {
     xmlhttp.open("POST", url, true);
     xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xmlhttp.send(parameters);
-}
+}*/
