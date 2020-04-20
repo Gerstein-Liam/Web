c133b8802edfdea9924a7c1ef9bd8ca1dddcfbb7
liam <liam.gerstein@gmail.com>
2020-04-20 22:11:55 +0200

    implement addperson

--- SITES/UFO1_PHP/add.php
+++ SITES/UFO1_PHP/add.php
@@ -5,9 +5,7 @@
 	$sector =htmlentities(trim($_GET['sector'])); 
   $country=htmlentities(trim($_GET['country'])); 
   $url=htmlentities(trim($_GET['preslink'])); 
-  
-  
-  
+ 
   $query= "INSERT INTO `person`(`ID_person`, `NAME`, `VORNAME`, `SECTOR`,`COUNTRY`,`PresLink`) VALUES (NULL,'" . $name . "','" . $vorname. "','" . $sector. "','" . $country. "','" . $url. "')";
   $result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 ?>
--- SITES/UFOV2/FRONTEND/js/admin.js
+++ SITES/UFOV2/FRONTEND/js/admin.js
@@ -1,6 +1,33 @@
 AddPersonModal= new Modal("AddPersonModal");
 
 
+
+function RequestAPI_PostMethod(id, url ,parameters) {
+   
+    console.log("requested URL: " + url);
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+   
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(this.responseText);
+
+        
+           // document.getElementById(id).innerHTML = this.responseText;
+        }
+    };
+    xmlhttp.open("POST", url, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xmlhttp.send(parameters);
+}
+
+
 function AddPerson(){
 
 
@@ -16,9 +43,11 @@ let country = document.getElementById("country").value;
 let implication = document.getElementById("implication").value;
 let position = document.getElementById("position").value;
 
-
+var paras = `command=addperson&LASTNAME=${lastname}&FIRSTNAME=${firstname}&FONCTION=${fonction}&DOMAIN=${domain}&COUNTRY=${country}&IMPLICATION=${implication}&POSITION=${position} `;
 
 console.log(`LASTNAME= ${lastname}  FIRSTNAME=${firstname}  FONCTION= ${fonction} `);
 console.log(`DOMAIN= ${domain}  COUNTRY=${country}  IMPLICATION= ${implication}  POSITION ${position} `);
+
+RequestAPI_PostMethod("bla", "/API" ,paras);
 
 }
