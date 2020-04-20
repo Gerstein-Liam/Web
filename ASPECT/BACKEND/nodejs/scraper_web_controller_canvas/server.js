a2153ef3ebf37642252a64b03cf3c2861bc4c1d2
liam <liam.gerstein@gmail.com>
2020-04-20 14:59:51 +0200

    improve_server_with_post

--- ASPECT/BACKEND/nodejs/scraper_web_controller_canvas/server.js
+++ ASPECT/BACKEND/nodejs/scraper_web_controller_canvas/server.js
@@ -1,9 +1,10 @@
+//npm install querystring
 var http = require('http');
+var querystring = require('querystring');
 var url = require('url');
 var htmlBuilder = require("./custom_node_modules/htmlBuilder");
 var hq = require("./custom_node_modules/html_queries");
 var capi = require("./custom_node_modules/api_request");
-const { Console } = require('console');
 htmlBuilder.HTMLBuild("index_desktop_seed.html","index_desktop.html");  
 http.createServer(function (req, res) {
   htmlBuilder.HTMLBuild("index_desktop_seed.html","index_desktop.html");  
@@ -17,16 +18,37 @@ http.createServer(function (req, res) {
                 }
     break;
     case "POST": console.log("Post Method");
+                WebApplication__PostRequests(req,res);
     break;
     default: break;
   }
 }).listen(8080);
 
-
-
-
-
-
+function WebApplication__PostRequests(req,res){
+  //https://nodejs.dev/making-http-requests-with-nodejs
+  let body = '';
+        // very important to handle errors
+        req.on('error', (err) => {
+            if(err) {
+                response.writeHead(500, {'Content-Type': 'text/html'});
+                response.write('An error occurred');
+                response.end();
+            }
+        });
+        // read chunks of POST data
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        // when complete POST data is received
+        req.on('end', () => {
+            // use parse() method
+            body = querystring.parse(body);
+            // { name: 'John', gender: 'MALE', email: 'john@gmail.com' }
+            console.log(body);
+            console.log(body.command);
+            // rest of the code
+        });
+}
 function WebApplication__GetRequests(req,res){
   if (req.url === ("/")) {
     //  console.log("request of index.html :  URL=" + req.url.substring(1));
