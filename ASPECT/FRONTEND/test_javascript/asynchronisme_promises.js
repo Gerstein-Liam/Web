3a3ba79624ae565ff9cf71b9a9e80f6e80a29935
liam <liam.gerstein@gmail.com>
2020-04-21 18:44:06 +0200

    sequential_promises

--- ASPECT/FRONTEND/test_javascript/asynchronisme_promises.js
+++ ASPECT/FRONTEND/test_javascript/asynchronisme_promises.js
@@ -1,5 +1,52 @@
+function AjaxGet_WithPromises(url) {
+    return new Promise(function (resolve, reject) {
+        var req = new XMLHttpRequest();
+        req.open('GET', url, true)
+        req.onreadystatechange = function () {
+            if (req.readyState == 4) {
+                if (req.status == 200) {
+                    resolve(this.responseText);
+                }
+                else {
+                    reject(req.status);
+                }
+            }
+        };
+        req.send(null);
+    }
+    )
+    //console.log(`function executed   ${url}`);
+}
+function a__OnSuccess_FirstJSON(success_reponse) {
+    var obj = JSON.parse(success_reponse);
+    console.log("On Success callback");
+    console.table(obj);
+    document.getElementById("FirstJson").innerHTML = success_reponse;
+}
+function a__OnSuccess_SecondJSON(success_reponse) {
+    var obj = JSON.parse(success_reponse);
+    console.log("On Success callback");
+    console.table(obj);
+    document.getElementById("SecondJson").innerHTML = success_reponse;
+}
+function Bt_PromisesTest() {
+    var ajax = AjaxGet_WithPromises;
+    var On_SuccessCallback_1 = a__OnSuccess_FirstJSON;
+    var On_SuccessCallback_2 = a__OnSuccess_SecondJSON;
+    ajax("https://jsonplaceholder.typicode.com/users")
+        .then(function (resolve_response_1) {
+            On_SuccessCallback_1(resolve_response_1);
+            return ajax("https://jsonplaceholder.typicode.com/posts");
+        })
+        .then(function (resolve_response_2) {
+            On_SuccessCallback_2(resolve_response_2);
+        })
+        .catch(function (reject_respect) {
+            console.log("One of the request faildes");
+        })
+    console.log("Still Running after inititing ajax");
+}
 /*    Second Step
-
 function AjaxGet_WithPromises(url) {
     return new Promise(function (resolve, reject) {
         var req = new XMLHttpRequest();
@@ -47,8 +94,6 @@ function Bt_PromisesTest() {
     });
     console.log("Still Running after inititing ajax");
 }
-
-
 */
 /*          Promise second step
 function AjaxGet_WithPromises(url) {
