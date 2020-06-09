6dc502a5fc7d3bcf5c7219e020a926bcac412b30
liam <liam.gerstein@gmail.com>
2020-06-09 22:32:41 +0200

    implementing add modal

--- SITES/UFOV2/FRONTEND/js/EventDB.js
+++ SITES/UFOV2/FRONTEND/js/EventDB.js
@@ -8,18 +8,21 @@ AddEventModal = new ShowHide_FixedPosition('AddEventModal',
         document.getElementById('add_event_modal').getElementsByClassName('db_status')[0].innerHTML = "IDLE";
     }
 );
-
-
 function AddEvent() {
     var event_info = {
-        NAME: `${document.getElementById('add_event_modal').getElementsByClassName('name')[0].value}}`,
-        DATE: `${document.getElementById('add_event_modal').getElementsByClassName('date')[0].value}`,
+        NAME: `${document.getElementById('add_event_modal').getElementsByClassName('name')[0].value}`,
+        DATE: `${document.getElementById('add_event_modal').getElementsByClassName('year')[0].value}-${document.getElementById('add_event_modal').getElementsByClassName('month')[0].value}-${document.getElementById('add_event_modal').getElementsByClassName('day')[0].value}`,
         COUNTRY: `${document.getElementById('add_event_modal').getElementsByClassName('country')[0].value}`,
         TYPE: `${document.getElementById('add_event_modal').getElementsByClassName('type')[0].value}`
       //  LATITUDE: `${document.getElementById("latitude").value}`,
       //  LONGITUDE:
-   
     };
+
+
+    console.log(event_info.DATE);
+
+
+    // Y : M : D
     //console.table([this._UpdateValues]);
     var post_para = {
         COMMAND: "ADD EVENT",
@@ -35,9 +38,7 @@ function AddEvent() {
     }, function (err) {
         document.getElementById('add_event_modal').getElementsByClassName('db_status')[0].style.backgroundColor = "red";
         document.getElementById('add_event_modal').getElementsByClassName('db_status')[0].innerHTML = err.ERROR;
-        alert(err);
+        //alert(err);
     });
     Ajax.ExecutePOST(null, null);
 }
-
-
